5ac7fe6570efd9cf6e3d385a69ad667acc20285b
peposlawa <peposlawa@gmail.com>
2024-08-08 11:58:08 +0100

    ticket 7 completed

--- tests/todos.test.js
+++ tests/todos.test.js
@@ -1,5 +1,9 @@
-import { test, expect } from "@playwright/test"
+import { test, expect } from "@playwright/test";
 
-test('first practice test', async ({ page }) => {
-    await page.goto('http://localhost:5432');
-})
+test("first practice test", async ({ page }) => {
+  await page.goto("http://localhost:5432");
+//   await expect(page.getByRole("heading", { name: "Todo List App" })).toBeVisible();
+  await page.getByLabel('New Todo:').fill('Drinking');
+  await expect(page.getByLabel('New Todo:')).toHaveValue('Drinking')
+//   await page.getByRole("button", { name: /submit/i }).click();
+});
